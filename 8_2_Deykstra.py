88c677c051b0c9f7e90878bccb854a7e33241b15
mmingalov <mmingalov@gmail.com>
2020-01-17 23:12:45 +1000

    8

---
+++ 8_2_Deykstra.py
@@ -0,0 +1,57 @@
+# 1. На улице встретились N друзей. Каждый пожал руку всем остальным друзьям (по одному разу). Сколько рукопожатий было?
+# Примечание. Решите задачу при помощи построения графа.
+# 2. Доработать алгоритм Дейкстры (рассматривался на уроке), чтобы он дополнительно возвращал список вершин, которые необходимо обойти.
+# 3. Написать программу, которая обходит не взвешенный ориентированный граф без петель, в котором все вершины связаны, по алгоритму поиска в глубину (Depth-First Search).
+# Примечания:
+# a. граф должен храниться в виде списка смежности;
+# b. генерация графа выполняется в отдельной функции, которая принимает на вход число вершин.
+
+
+def dijkstra(graph, start):
+    length = len(graph)
+    is_visited = [False] * length
+    cost = [float('inf')] * length
+    parent = [-1] * length
+    points_dict = {}
+    cost[start] = 0
+    min_cost = 0
+
+    while min_cost < float('inf'): #обходим граф в цикле
+
+        is_visited[start]=True
+
+        for i,vertex in enumerate(graph[start]): #проходим по строке матрицы смежности графа где хранится значение start
+            list = []#set()
+            # list.append(start)
+            if vertex != 0 and not is_visited[i]:
+                #есть ребро и вершину еще не посещали
+                list.append(start) #add(i)
+                points_dict[i] = list
+                if cost[i] > vertex + cost[start]:
+                    cost[i] = vertex + cost[start] # в i вершине новое, более короткое расстояние
+                    parent[i] = start #какая вершина является родительской
+                    list.append(i)#add(start)
+                    points_dict[i] = list
+
+        min_cost = float('inf')
+        for i in range(length):
+            if min_cost > cost[i] and not is_visited[i]:
+                min_cost = cost[i]
+                start = i
+                # list.append(i)  # add(start)
+                # points_dict[i] = list
+    return cost,points_dict
+
+g = [
+    [0,0,1,1,9,0,0,0],
+    [0,0,9,4,0,0,5,0],
+    [0,9,0,0,3,0,6,0],
+    [0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,1,0],
+    [0,0,0,0,0,0,5,0],
+    [0,0,7,0,8,1,0,0],
+    [0,0,0,0,0,1,2,0],
+
+]
+s = int(input('От какой вершины идти: '))
+print(dijkstra(g, s))
